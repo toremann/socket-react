4f39af41ff89d12f311ad41c95a849478149c8c6
toremann <50668920+toremann@users.noreply.github.com>
2022-07-13 00:21:22 +0200

    template
    socket.io test

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input placeholder="Message..." />
+      <button>Send message...</button>
+    </div>
+  );
+}
+
+export default App;
